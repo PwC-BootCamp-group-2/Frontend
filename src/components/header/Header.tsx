86ca7b8b47526fc6b02aa815e51e242edd9bcfc7
sundaygabriel <gabrielchinedus@gmail.com>
2022-08-20 09:53:38 +0100

    Start

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,53 @@
+import spacehub from "../../assets/img/spacehub.png";
+import { Link } from "react-router-dom";
+import { FaUser, FaBars, FaUserAlt, FaTimes } from "react-icons/fa";
+import { Badge } from "react-bootstrap";
+import "./Header.css";
+import { useState } from "react";
+
+const Header = () => {
+  // Get the state of the navbar
+  const [navList, setNavList] = useState(false);
+
+  return (
+    <header>
+      <nav className="container flex">
+        <div className="logo">
+          <img src={spacehub} />
+        </div>
+
+        <div className="nav">
+          <ul className={navList ? "small" : "flex"}>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/howitworks">How it Works</Link>
+            </li>
+            <li>
+              <Link to="/becomeahost">Become a Host</Link>
+            </li>
+            <li>
+              <Link to="/pricing">Pricing</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div className="acct-div">
+          <button className="btn btn-account">
+            <FaUserAlt className="btn-icon" />
+            <span>Account</span>
+          </button>
+        </div>
+
+        <div className="toggle">
+          <button onClick={() => setNavList(!navList)} className="btn-menu">
+            {navList ? <FaTimes color="orangered" /> : <FaBars />}
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/howitworks/HowItWorks.tsx
@@ -0,0 +1,10 @@
+
+const HowItWorks = () => {
+  return (
+    <main className='container'>
+      <h1>How it works</h1>
+    </main>
+  );
+}
+
+export default HowItWorks
