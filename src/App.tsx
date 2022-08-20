b401784df706c3516c5d86aca4bb58d4c83d5586
sundaygabriel <gabrielchinedus@gmail.com>
2022-08-20 10:54:01 +0100

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
+import Home from './pages/Home'
+import About from './pages/about/About'
+import BecomeAHost from './pages/becomeahost/BecomeAHost'
+import Pricing from './pages/pricing/Pricing'
+import HowItWorks from './pages/howitworks/HowItWorks'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+        
+      <Routes>
+        <Route path='/' element = {<Home />} />
+        <Route path='/about' element = {<About />} />
+        <Route path='/becomeahost' element = {<BecomeAHost />} />
+        <Route path='/pricing' element = {<Pricing />} />
+        <Route path='/howitworks' element = {<HowItWorks />} />
+      </Routes>
+      
+      <Footer />
     </div>
   );
 }
