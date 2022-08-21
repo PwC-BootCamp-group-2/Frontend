4c0fb3c1b2c13d593413015b1b320608806efa4b
Royalboe <auomidejohnson@gmail.com>
2022-08-21 11:16:32 +0100

    Fixed conflicts and added images to About Us

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Home from './pages/Home';
-import About from './pages/about/About';
+import AboutUs from './pages/about/AboutUs';
 import Pricing from './pages/pricing/Pricing';
 import BecomeAHost from './pages/becomeahost/BecomeAHost';
 import HowItWorks from './pages/howitworks/HowItWorks';
@@ -17,7 +17,7 @@ function App() {
     
         <Routes>
           <Route path={"/"} element={<Home />} />
-          <Route path="/about" element={<About/>} />
+          <Route path="/about" element={<AboutUs/>} />
           <Route path={"/becomeahost"} element={<BecomeAHost />} />
           <Route path={"/pricing"} element={<Pricing />} />
           <Route path={"/howitworks"} element={<HowItWorks />} />
