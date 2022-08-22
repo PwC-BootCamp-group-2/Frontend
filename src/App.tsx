b68c17157496eafdcbaaa2c122595302dedebe4a
Royalboe <auomidejohnson@gmail.com>
2022-08-22 11:26:50 +0100

    Fix: fixed conlicts

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-<<<<<<< HEAD
-import Footer from './components/footer/Footer';
-import Header from './components/header/Header';
-import AboutUs from './pages/about/AboutUs';
-import Home from './pages/home/Home';
+import Footer from './website/components/footer/Footer';
+import Header from './website/components/header/Header';
+import AboutUs from './website/pages/about/AboutUs';
+import Home from './website/pages/home/Home';
 // import About from './pages/about/About';
-import Pricing from './pages/pricing/Pricing';
-import BecomeAHost from './pages/becomeahost/BecomeAHost';
-import HowItWorks from './pages/howitworks/HowItWorks';
-import Account from './pages/account/createAccount/Account';
-import Login from './pages/account/login/Login';
+import Pricing from './website/pages/pricing/Pricing';
+import BecomeAHost from './website/pages/becomeahost/BecomeAHost';
+import HowItWorks from './website/pages/howitworks/HowItWorks';
+import Account from './website/pages/account/createAccount/Account';
+import Login from './website/pages/account/login/Login';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './assets/css/style1.css'
 import './assets/css/style2.css'
 import './assets/css/style3.css'
-import UserSignup from './pages/account/usersignup/UserSignup';
-import TeamSignup from './pages/account/teamsignup/TeamSignup';
+// old
+import './assets/css/damier.css'
+import './assets/css/leke.css'
+import './assets/css/sarah.css'
+import UserSignup from './website/pages/account/usersignup/UserSignup';
+import TeamSignup from './website/pages/account/teamsignup/TeamSignup';
 
 function App() {
   return (
     <main className="App">
       <Header />
-    
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<AboutUs/>} />
@@ -34,36 +36,6 @@ function App() {
         </Routes>
       <Footer />
     </main>
-=======
-import './App.css';
-import Header from './components/header/Header';
-import Footer from './components/footer/Footer';
-import Home from './pages/Home'
-import About from './pages/about/About'
-import BecomeAHost from './pages/becomeahost/BecomeAHost'
-import Pricing from './pages/pricing/Pricing'
-import HowItWorks from './pages/howitworks/HowItWorks'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import './assets/css/damier.css'
-import './assets/css/leke.css'
-import './assets/css/sarah.css'
-
-function App() {
-  return (
-    <div className="App">
-      <Header />
-        
-      <Routes>
-        <Route path='/' element = {<Home />} />
-        <Route path='/about' element = {<About />} />
-        <Route path='/becomeahost' element = {<BecomeAHost />} />
-        <Route path='/pricing' element = {<Pricing />} />
-        <Route path='/howitworks' element = {<HowItWorks />} />
-      </Routes>
-      
-      <Footer />
-    </div>
->>>>>>> origin/gabriel
   );
 }
 
