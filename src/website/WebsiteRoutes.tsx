21c779f5036f7ace925a787f117c22fa39664cd3
Anagho okiroro Obruthe <anaghosamroyce@gmail.com>
2022-08-22 16:38:36 +0100

    Hero section

--- src/website/WebsiteRoutes.tsx
+++ src/website/WebsiteRoutes.tsx
@@ -4,10 +4,7 @@ import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import AboutUs from './pages/about/AboutUs';
 import Home from './pages/home/Home';
-// import About from './pages/about/About';
-import Pricing from './pages/pricing/Pricing';
-import BecomeAHost from './pages/becomeahost/BecomeAHost';
-import HowItWorks from './pages/howitworks/HowItWorks';
+
 import Account from './pages/account/createAccount/Account';
 import Login from './pages/account/login/Login';
 import './assets/css/style1.css';
@@ -28,15 +25,12 @@ const WebsiteRoutes: FC = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<AboutUs />} />
-          <Route path="/becomeahost" element={<BecomeAHost />} />
-          <Route path="/pricing" element={<Pricing />} />
-          <Route path="/howitworks" element={<HowItWorks />} />
           <Route path="/account" element={<Account />} />
           <Route path="/login" element={<Login />} />
           <Route path="/usersignup" element={<UserSignup />} />
           <Route path="/teamsignup" element={<TeamSignup />} />
         </Routes>
-        <Footer />
+        {/* <Footer /> */}
       {/* </BrowserRouter> */}
     </>
   );
